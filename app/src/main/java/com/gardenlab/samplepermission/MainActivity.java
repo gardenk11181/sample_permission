baae02d7051fc0be927b880645865627af23127f
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-02-20 11:40:16 +0900

    Only use write_external_storage

--- app/src/main/java/com/gardenlab/samplepermission/MainActivity.java
+++ app/src/main/java/com/gardenlab/samplepermission/MainActivity.java
@@ -25,32 +25,34 @@ public class MainActivity extends AppCompatActivity {
         };
 
         checkPermissions(permissions);
+
     }
 
     public void checkPermissions(String[] permissions) {
         ArrayList<String> targetList = new ArrayList<String>();
 
         for(int i=0; i<permissions.length; i++) {
+            Log.d("targets",":"+i);
             String curPermission = permissions[i];
             int permissionCheck = ContextCompat.checkSelfPermission(this,curPermission);
             if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, curPermission+"권한 있음.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, curPermission+"권한 있음.", Toast.LENGTH_LONG).show();
             } else {
-                Toast.makeText(this, curPermission+"권한 없음", Toast.LENGTH_SHORT).show();
-                if(ActivityCompat.shouldShowRequestPermissionRationale(this,curPermission)) {
-                    // 사용자가 이전에 권한 거부하여 권한 획득 이유 설명하기 위함. (권한 재요청)
-                    // 다시묻지 않기 선택시 이 메서드 상관없이 거부할 때랑 동일한 콜백 함수 실행
-                    Toast.makeText(this,curPermission+"권한 설명 필요", Toast.LENGTH_SHORT).show();
-                } else {
-                    targetList.add(curPermission);
-                }
+                Toast.makeText(this, curPermission+"권한 없음", Toast.LENGTH_LONG).show();
+//                if(ActivityCompat.shouldShowRequestPermissionRationale(this,curPermission)) {
+//                    // 사용자가 이전에 권한 거부하여 권한 획득 이유 설명하기 위함. (권한 재요청)
+//                    // 다시묻지 않기 선택시 이 메서드 상관없이 거부할 때랑 동일한 콜백 함수 실행
+//                    Toast.makeText(this,curPermission+"권한 설명 필요", Toast.LENGTH_SHORT).show();
+//                } else {
+//                    targetList.add(curPermission);
+//                }
+                targetList.add(curPermission);
             }
         }
-
         String[] targets = new String[targetList.size()];
         targetList.toArray(targets);
 
-//        ActivityCompat.requestPermissions(this,targets,101);
+        ActivityCompat.requestPermissions(this,targets,101);
     }
 
     @Override
@@ -61,6 +63,12 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(this, "첫번째 권한을 사용자가 승인함", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(this, "첫번째 권한 거부됨", Toast.LENGTH_SHORT).show();
+                }
+
+                if(grantResults.length > 0 && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "두번째 권한을 사용자가 승인함", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "두번째 권한 거부됨", Toast.LENGTH_SHORT).show();
                 }
 
                 return;
